9c0e4e0b700fb6ffaadb7b462da4fe3ef8f3aaf9
visa4 <antonino.visalli@gmail.com>
2023-01-25 15:10:09 +0100

    Fix video panel

--- app/elements/layout/application-layout.js
+++ app/elements/layout/application-layout.js
@@ -136,7 +136,7 @@ class ApplicationLayout extends AclMixin(LocalizeMixin(ServiceInjectorMixin(Poly
             section: {
                 type: String,
                 notify: true,
-                value : 'resource',
+                value : 'video-panel',
                 observer: 'changeSection'
             },
 
